d4fcaa632b34a8de07330f64f2c54276116595c4
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2021-09-25 13:24:45 -0700

    added user and login functionality

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,13 +1,43 @@
 const { User, Book } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
 
 const resolvers = {
   Query: {
-    users: async () => {
+    me: async () => {
       return User.find().select("-__v -password");
     },
-    books: async (parent, { username }) => {
-      const params = username ? { username } : {};
-      return Book.find(params).sort({ createdAt: -1 });
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+
+      return user;
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      return user;
+    },
+    saveBook: async (parent, args) => {
+      const book = await Book.create(args);
+
+      return book;
+    },
+    removeBook: async (parent, args) => {
+      const book = await Book.create(args);
+
+      return book;
     },
   },
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,27 +3,39 @@ const { gql } = require("apollo-server-express");
 
 // create typedefs
 const typeDefs = gql`
-
   type User {
     _id: ID
-    username: String
+    username: String!
     email: String
+    bookCount: Int
     savedBooks: [Book]
   }
 
   type Book {
-    _id: ID  
-    authors: String
+    bookId: ID!
+    authors: [String]
     description: String
+    title: String
     image: String
     link: String
-    title: String
   }
 
   type Query {
-    users: [User]
-    user(username:String!): User  
-    books(username: String): [Book]
+    me: User
+  }
+
+  type Mutation {
+      login(email: String!, password: String!): User
+      addUser(username: String!, email: String!, password: String!): User
+  }
+
+  input bookData {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
   }
 `;
 
